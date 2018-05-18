97679eeadc8ce2c321ea3dbe4dc3f4d7433a59f8
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-18 03:20:56 +0200

    editor added

--- quickTranslate.js
+++ quickTranslate.js
@@ -1,5 +1,6 @@
 window.onload = start;
 
+var elements = [];
 function start() {
 	// init select
     $(document).ready(function() {
@@ -11,12 +12,45 @@ function start() {
 
 		// set data to iframe and hide
 		document.getElementById("iframe").style.display = "none";
-		document.getElementById("iframe").innerHTML = data.html;
+		document.getElementById("iframe").contentDocument.write(data.html);
+		console.log(document.getElementById("iframe"));
 
-		// get every line from JSON and convert to HTML elements
-		let split = data.html.split("<");
-		split.forEach(line => {
-			console.log("<" + line);
-		})
+		// remove all scripts and styles and store for later
+		let frame = document.getElementById("iframe").contentDocument.childNodes[1];
+
+		let text = frame.querySelectorAll("body");
+		let count = 0;
+		text.forEach(child => {
+			elements.push(child);
+		});
+
+		console.log(elements);
+		translate();
 	});
+}
+
+var textField;
+var eleCounter;
+var currentEle = 0;
+function translate() {
+	textField = document.getElementById("translateText");
+	textField.value = elements[currentEle].textContent;
+	removeWhiteSpace();
+	textField.value = removeWhiteSpace();
+
+	// init ok translate event
+	document.getElementById("nextElement").addEventListener("click", translateOk);
+}
+
+function translateOk() {
+
+
+}
+
+function removeWhiteSpace() {
+	let s = textField.value;
+	s = s.replace(/(^\s*)|(\s*$)/gi,"");
+	s = s.replace(/[ ]{2,}/gi," ");
+	s = s.replace(/\n /,"\n");
+	return s;
 }
