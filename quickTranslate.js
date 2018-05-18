c5438eb3e4cd5d3428ed60f161b85fc973c44891
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-18 04:06:35 +0200

    it works

--- quickTranslate.js
+++ quickTranslate.js
@@ -1,6 +1,8 @@
 window.onload = start;
 
 var elements = [];
+var from;
+var to;
 function start() {
 	// init select
     $(document).ready(function() {
@@ -9,6 +11,11 @@ function start() {
 
     // get the data from translator.js
 	$.get("/translator/fileData", {}, function(data){
+
+		// set from and to languages
+		from = data.from;
+		to = data.to;
+		console.log(to);
 
 		// set data to iframe and hide
 		document.getElementById("iframe").style.display = "none";
@@ -30,13 +37,14 @@ function start() {
 }
 
 var textField;
-var eleCounter;
-var currentEle = 0;
 function translate() {
+
+	// run yandex translator
 	textField = document.getElementById("translateText");
-	textField.value = elements[currentEle].textContent;
+	textField.value = elements[0].textContent;
 	removeWhiteSpace();
 	textField.value = removeWhiteSpace();
+	translator(textField.value);
 
 	// init ok translate event
 	document.getElementById("nextElement").addEventListener("click", translateOk);
@@ -53,4 +61,14 @@ function removeWhiteSpace() {
 	s = s.replace(/[ ]{2,}/gi," ");
 	s = s.replace(/\n /,"\n");
 	return s;
+}
+
+function translator(text) {
+	// get the data from translator.js
+	$.get("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+		+ "key=trnsl.1.1.20180518T012329Z.b73950d2865d8de5.dda04c58ef76240ad5589baf431e12f8f1f5a0bd"
+		+ "&lang=" + to + "&text=" + text, {}, function(data) {
+			textField.value = data.text;
+			console.log(data);
+	});
 }
