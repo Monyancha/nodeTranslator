ca53bcfaef57c08b649486d16b8eb0692c6b515c
Sanderhelleso <sanderhelleso@hotmail.com>
2018-05-18 05:03:17 +0200

    now instantly translates each text node, can go forward / back

--- quickTranslate.js
+++ quickTranslate.js
@@ -3,6 +3,7 @@ window.onload = start;
 var elements = [];
 var from;
 var to;
+var frame;
 function start() {
 	// init select
     $(document).ready(function() {
@@ -23,12 +24,18 @@ function start() {
 		console.log(document.getElementById("iframe"));
 
 		// remove all scripts and styles and store for later
-		let frame = document.getElementById("iframe").contentDocument.childNodes[1];
+		frame = document.getElementById("iframe").contentDocument.childNodes[1];
 
-		let text = frame.querySelectorAll("body");
+		// get all nodes
+		let text = frame.querySelectorAll("*");
 		let count = 0;
 		text.forEach(child => {
-			elements.push(child);
+			count++;
+			// set class for referance
+			child.classList.add("translatedElement" + count);
+
+			// run html validation
+			checkHTML(child);
 		});
 
 		console.log(elements);
@@ -36,33 +43,36 @@ function start() {
 	});
 }
 
+// set up the translator
 var textField;
+var elementCounter = 1;
 function translate() {
 
 	// run yandex translator
+	document.getElementById("eleCount").innerHTML = "Element " + elementCounter + " of " + elements.length;
 	textField = document.getElementById("translateText");
-	textField.value = elements[0].textContent;
-	removeWhiteSpace();
-	textField.value = removeWhiteSpace();
-	translator(textField.value);
+	translator(elements[elementCounter - 1].innerHTML);
 
 	// init ok translate event
 	document.getElementById("nextElement").addEventListener("click", translateOk);
 }
 
+// update 
 function translateOk() {
 
+	// remove event if no more text to translate
+	if (elementCounter === elements.length) {
+		this.removeEventListener("click", translateOk);
+	}
 
+	else {
+		elementCounter++;
+		translator(elements[elementCounter - 1].innerHTML);
+		document.getElementById("eleCount").innerHTML = "Element " + elementCounter + " of " + elements.length;
+	}
 }
 
-function removeWhiteSpace() {
-	let s = textField.value;
-	s = s.replace(/(^\s*)|(\s*$)/gi,"");
-	s = s.replace(/[ ]{2,}/gi," ");
-	s = s.replace(/\n /,"\n");
-	return s;
-}
-
+// translate selected text
 function translator(text) {
 	// get the data from translator.js
 	$.get("https://translate.yandex.net/api/v1.5/tr.json/translate?"
@@ -71,4 +81,13 @@ function translator(text) {
 			textField.value = data.text;
 			console.log(data);
 	});
+}
+
+// check if text is of HTML
+function checkHTML(child) {
+	let test = /<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\/\2>/i.test(child.innerHTML);
+	if (test === false && child.innerHTML != "" && child.innerHTML != " ") {
+		// push to array if ok
+		elements.push(child);
+	}
 }
